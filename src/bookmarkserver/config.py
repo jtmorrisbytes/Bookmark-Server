2d6af7c6cb0d896931c1e5df13d69946d13dd29e
Jordan T Morris <jthecybertinkerer@gmail.com>
2019-02-21 06:12:15 -0600

    trying to deploy with waitress

--- src/bookmarkserver/config.py
+++ src/bookmarkserver/config.py
@@ -1,11 +1,13 @@
 import os
 import secrets
-if os.environ.get("FLASK_ENV").lower() == 'development':
+if os.environ.get("FLASK_ENV",'').lower() == 'development':
     DATABASE_URL = os.environ.get("DATABASE_URL", 'sqlite:///:memory:')
     DEBUG=True
     EXPLAIN_TEMPLATE_LOADING = os.environ.get('EXPLAIN_TEMPLATE_RELOADING')
 else:
     DATABASE_URL = os.environ.get("DATABASE_URL", 'sqlite:///bookmarks.db')
+ENV = os.environ.get("ENV", 'production')
 SECRET_KEY = secrets.token_hex(64)
+PORT=int(os.environ.get("PORT",80))
 SQLALCHEMY_DATABASE_URI = DATABASE_URL
 CONNECTION_TIMEOUT_LIMIT = int(os.environ.get('CONNECTION_TIMEOUT_LIMIT', 15))
--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
 from bookmarkserver.app import bookmark_server
+from waitress import serve
 if __name__ == "__main__":
-    bookmark_server.run(host='0.0.0.0',)
+    ENV=bookmark_server.config.get("ENV",'').lower()
+    port = bookmark_server.config.get("PORT")
+    if ENV =='development' or ENV =='testing':
+        bookmark_server.run(host='0.0.0.0',port=port)
+    else:
+        serve(bookmark_server,host='0.0.0.0', port=port)
